46b05b846378877d2887431561c68d00a5f4714f
Aisha Gench <aisha.gench@gmail.com>
2021-07-08 20:38:43 -0500

    install npm install --save redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import {createStore, applyMiddleware} from 'redux'
 import rootReducer from './reducers/rootReducer'
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const myStore = createStore(rootReducer, applyMiddleware(thunk))
+const myStore = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 ReactDOM.render(
   <React.StrictMode>
     <App />
