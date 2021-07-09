1c76a07a2ef1d948e02d1602d135db61e10f2464
Aisha Gench <aisha.gench@gmail.com>
2021-07-08 19:52:22 -0500

    import createStore

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// eslint-disable-next-line no-unused-vars
+import {createStore} from 'redux'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +13,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
