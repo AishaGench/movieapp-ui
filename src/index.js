e7d6b672c0083d5a52866bec1e7aba070d672082
Aisha Gench <aisha.gench@gmail.com>
2021-07-08 20:44:02 -0500

    import provider

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ import {createStore, applyMiddleware} from 'redux'
 import rootReducer from './reducers/rootReducer'
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
+import {Provider} from "react-redux"
 const myStore = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
 ReactDOM.render(
-  <React.StrictMode>
+
+  <Provider store={myStore}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
+
   document.getElementById('root')
 );
 
