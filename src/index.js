3fc4457e2e1665192f1c5d6fde1463c375fa7907
Aisha Gench <aisha.gench@gmail.com>
2021-07-08 19:58:18 -0500

    create rootReducer

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// eslint-disable-next-line no-unused-vars
 import {createStore} from 'redux'
+import rootReducer from './reducers/rootReducer'
 
+const myStore = createStore(rootReducer)
 ReactDOM.render(
   <React.StrictMode>
     <App />
