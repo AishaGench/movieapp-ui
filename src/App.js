1972107ae68bb7be2a031e316e4b7b8055e20296
Aisha Gench <aisha.gench@gmail.com>
2021-07-08 21:01:40 -0500

    import link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
+import {Link} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       App Component
+      <br/>
+      <Link to="movies">Movies</Link>
     </div>
   );
 }
