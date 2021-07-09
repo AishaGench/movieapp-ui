27f608b7067232b4e481a73c852722887bb1fa65
Aisha Gench <aisha.gench@gmail.com>
2021-07-08 20:30:38 -0500

    import applyMiddleware

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import rootReducer from './reducers/rootReducer'
 import thunk from 'redux-thunk';
 
-const myStore = createStore(rootReducer, thunk)
+const myStore = createStore(rootReducer, applyMiddleware(thunk))
 ReactDOM.render(
   <React.StrictMode>
     <App />
