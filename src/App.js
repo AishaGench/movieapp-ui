a4bc4c595a227673de010c733e85fdf6491170f1
Aisha Gench <aisha.gench@gmail.com>
2021-07-10 12:11:22 -0500

    install semantic ui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import {Link, Route} from 'react-router-dom'
 import MoviesPage from './components/pages/MoviesPage';
+import React, { Component } from 'react'
+import {
+  Container,Divider,Dropdown,Grid,Header,Icon,Image,List,Menu,Segment,Visibility,
+} from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render() {
+    return (
+      <div>
       App Component
       <br/>
       <Link to="movies">Movies</Link>
       <Route path='/movies' component={MoviesPage}></Route>
-    </div>
-  );
+      </div>
+    )
+  }
 }
-
 export default App;
